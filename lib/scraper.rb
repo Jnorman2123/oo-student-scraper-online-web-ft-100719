419f7a1e024079ad70c4f5293acbc508d31412c2
Jonathan Norman <jnorman2123@gmail.com>
2019-10-28 20:45:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,11 @@ class Scraper
     html = open(profile_url)
     profile = {}
     doc = Nokogiri::HTML(html)
-    doc.css("div.vitals-container").css("div.social-icon-container").css("a").attribute("href").value
+    doc.css("div.vitals-container").css("div.social-icon-container").css("a").attribute("href").value.each do |attribute| 
+      socials = []
+      socials << attribute 
+    end 
+    socials
     binding.pry
   end
 
